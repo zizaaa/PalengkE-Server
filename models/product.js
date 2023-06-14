5813aa03bb6600eaa52dd4112999dad0ef53cbf7
zizaaa <winfourteen@gmail.com>
2023-06-14 11:30:37 +0800

    update

--- models/product.js
+++ models/product.js
@@ -6,9 +6,38 @@ const productSchema = mongoose.Schema(
             type:String,
             required:[true,"Please Enter a product name"]
         },
+        quantity:{
+            type:Number,
+            require:true,
+            default:0,
+        },
+        newPrice:{
+            type:Number,
+            required:false
+        },
+        salePercentage:{
+            type:Number,
+            required:false,
+        },
         price:{
             type:Number,
             required: true,
+        },
+        quantity:{
+            type:String,
+            required:true,
+        },
+        bestSeller:{
+            type:Boolean,
+            default:false
+        },
+        sale:{
+            type:Boolean,
+            default:false
+        },
+        category:{
+            type:String,
+            required:true
         },
         img:{
             type:String,
